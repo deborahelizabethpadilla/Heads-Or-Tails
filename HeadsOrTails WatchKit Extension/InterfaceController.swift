f47cddf8b04ff1996986130ca509085e1450485b
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-01-31 21:43:18 -0500

    First Commit

---
+++ HeadsOrTails WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.swift
+//  HeadsOrTails WatchKit Extension
+//
+//  Created by Deborah on 1/31/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    
+    
+    @IBOutlet var answerLabel: WKInterfaceLabel!
+
+    @IBAction func flipButton() {
+        
+    }
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
